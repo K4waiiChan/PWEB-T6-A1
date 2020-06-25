201cd7b04d25f2b399c809656b16eba72f726dcb
Fernando Soto <fernando.soto@fundacion-jala.org>
2020-06-25 09:52:07 -0400

    Add inputs to intereact with external data

--- src/app/components/slides/simple-text/simple-text.component.ts
+++ src/app/components/slides/simple-text/simple-text.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { TextModel } from 'src/app/models/text.model';
 
 @Component({
   selector: 'app-simple-text',
@@ -6,10 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./simple-text.component.scss']
 })
 export class SimpleTextComponent implements OnInit {
-
+  @Input() content: TextModel;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getSize(): string {
+    return `${this.content.textSize}px`;
+  }
+  
+  getStyle(): string {
+    return this.content.textStyle;
+  }
+  
+  getWeight(): string {
+    return this.content.textWeight;
+  }
+
+  getFamily(): string {
+    return this.content.fontFamily;
+  }
+
+  getDecoration(): string {
+    return this.content.textDecoration;
+  }
 }
